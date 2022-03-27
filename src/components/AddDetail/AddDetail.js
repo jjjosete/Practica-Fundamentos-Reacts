f096a960c179e48835a26daf35bdcad4b2693d66
jjjosete <jfernan0001@gmail.com>
2022-03-27 21:39:27 +0200

    GET AddDetail - not ready

--- src/components/AddDetail/AddDetail.js
+++ src/components/AddDetail/AddDetail.js
@@ -1,13 +1,31 @@
 import Page from'../layout/page.js'
+import client from '../../API/client.js';
 import { useParams } from "react-router-dom";
-const AddDetail = () => {
+import { useState } from 'react/cjs/react.production.min';
+import { useEffect } from 'react/cjs/react.production.min';
+
+    const baseUrl = '/api/v1/adverts';
+ const getAdd = addId => {
+    const url = `${baseUrl}/${addId}`;
+    return client.get(url);
+  };
+  
+  
+  
+  const AddDetail = () => {
+
+    const paramsObj = useParams();
+    const params = JSON.stringify(paramsObj.addId);
     
-    const params = useParams();
-    console.log(params)
+    
+    const {add }= getAdd(params);
+   
     return (
-        <Page title={"Detalle de anuncio"}>
-<div> Esto es la página del detalle de anuncio </div>
-        </Page>
+    <Page title={'Detalle de anuncio'} >
+    <div className="addsPage">
+        Esto es el detalle de anuncio
+          </div>
+          </Page>
     )
 }
 
--- src/components/AddsPage/AddsPage.js
+++ src/components/AddsPage/AddsPage.js
@@ -2,6 +2,18 @@ import { useEffect, useState } from "react"
 import { getLatestAdds } from "./AddsService.js";
 import { Link, } from "react-router-dom";
 import Page from "../layout/page.js";
+import Button from "../common/Button.js";
+
+
+
+const EmptyList = () => (
+  <div style={{ textAlign: 'center' }}>
+    <p>Sé el primero en publicar un anuncio!</p>
+    <Button as={Link} to="/adverts/new" variant="primary">
+      Nuevo anuncio
+    </Button>
+  </div>
+);
 
 const AddsPage = () => {
     let [adds, setAdds] = useState([]);
@@ -19,27 +31,31 @@ const AddsPage = () => {
       <Page title={'Lista de anuncios'} >
         <div className="addsPage">
          
-          
+          {adds.length ? (
+
+              <ul>
+              {adds.map((add) => (
+                <li key={add.id}>
+                      
+                  <Link to={`/adverts/${add.id}`}>
+                  <h1>{add.name}</h1>
+                  <img src={add.photo} alt="Foto del anuncio"></img>
+                  <p>
+                    <b>Precio: </b>
+                    {add.price}€
+                  </p>
+                  <p>
+                    <b>Tags: </b>
+                    {add.tags}
+                  </p>
+              </Link>
+                </li>
+              ))}
+              </ul>
+          ):(
+            <EmptyList />
+          )}
          
-          <ul>
-            {adds.map((add) => (
-              <li key={add.id}>
-                    
-                <Link to={`/adverts/${add.id}`}>
-                <h1>{add.name}</h1>
-                <img src={add.photo} alt="Foto del anuncio"></img>
-                <p>
-                  <b>Precio: </b>
-                  {add.price}€
-                </p>
-                <p>
-                  <b>Tags: </b>
-                  {add.tags}
-                </p>
-             </Link>
-              </li>
-            ))}
-          </ul>
         </div>
       </Page>
     );
